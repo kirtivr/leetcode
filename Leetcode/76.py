05be58797ff8d81a59086504638ae30340a85ccd
Kirti Rathore <kirtivr.bit@gmail.com>
2023-06-13 20:15:31 +0530

    76 Minimum window substring

--- Leetcode/76.py
+++ Leetcode/76.py
@@ -1,3 +1,7 @@
+from heapq import heappop, heappush, heapify
+from dataclasses import dataclass, field
+from typing import Any
+
 class Solution:
     def minWindow(self, s, t):
         """
@@ -14,43 +18,79 @@ class Solution:
         for i in range(T):
             table[t[i]] = 1 if t[i] not in table else table[t[i]] + 1
 
-        prev = None
-        window = None
         oldT = dict(table)
-        
+        indices = []        
         for i in range(S):
-            if table.get(s[i],None):
-                table[s[i]] -= 1
-                if table[s[i]] == 0:
-                    table.pop(s[i])
+            if s[i] in table:
+                indices.append(i)
+
+        seen = {key : [] for key in table.keys()}
+        #print(seen)
+        indices_added = []
+        invalid_indices = {}
+        out = None
+
+        for i in indices:
+            #print(f'\ns = {s} processed = {s[:i + 1]} remaining = {s[i + 1 :]} i = {i} incoming = {s[i]} table = {table}')
+            # Either evict the earliest index or add s[i] to seen.
+            if s[i] not in table:
+                # Evict.
+                evicted = seen[s[i]][0]
+                #print(f'evicting {s[i]} at index {evicted}')
+                seen[s[i]].pop(0)
+                invalid_indices[evicted] = True
+                seen[s[i]].append(i)
+                indices_added.append(i)
+                continue
+
+            seen[s[i]].append(i)
+            indices_added.append(i)
+
+            table[s[i]] -= 1
+            if table[s[i]] == 0:
+                del table[s[i]]
+                if len(table) == 0:
+                    # Find the first valid index.
+                    first = None
+                    for j in range(len(indices_added)):
+                        if indices_added[j] not in invalid_indices:
+                            first = indices_added[j]
+                            break
+                    indices_added = indices_added[j + 1 :]
+                    seen[s[first]].pop(0)
+
+                    #print(f'first = {first} i = {i} invalid = {invalid_indices} candidate {s[first : i + 1]}')
+                    if out:
+                        if i - first + 1 < len(out):
+                            out = s[first : i + 1]
+                    else:
+                        out = s[first : i + 1]
+
+                    if s[first] not in table:
+                        table[s[first]] = 1
+                    else:
+                        table[s[first]] += 1
                     
-                if prev == None:
-                    prev = i
-                print(table)
-
-            if len(table) == 0 and (window == None or window[1] - window[0] + 1 > i+1 - prev):
-                window = (prev,i+1)
-                table = dict(oldT)
-                table[s[i]] -= 1
-                if table[s[i]] == 0:
-                    table.pop(s[i])
-                prev = i
-                print(table)
-
-        return s[window[0]:window[1]] if window else ""
-
+                    continue
+            
+        return out if out is not None else ""
+                
+                
 
 if __name__ == '__main__':
     S = "ADOBECODEBANC"
     T = "ABC"
-    S = "acbbaca"
+    '''S = "acbbaca"
     T = "aba"
     S = "cabefgecdaecf"
     T = "cae"
     S = "bdab"
     T = "ab"
     S = "aaflslflsldkalskaaa"
-    T = "aaa"
+    T = "aaa"'''
+    
+    S = "adobecodebancbbcaa"
+    T = "abc"
     print(Solution().minWindow(S,T))
             
                 
