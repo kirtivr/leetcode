65eb1ab3a0ef0a8d5a570fe1425489760e570a42
kirtivr <kirtivr.bit@gmail.com>
2023-09-17 00:25:50 +0530

    UTF-8 parser using Rust

--- Leetcode/rust393/src/main.rs
+++ Leetcode/rust393/src/main.rs
@@ -1,8 +1,77 @@
 struct Solution {}
 
 impl Solution {
+    pub fn bytes_to_read_given_preamble(preamble: i32) -> Result<usize, &'static str> {
+        // Return the number of consecutive ones starting from the MSB to the LSB.
+        let mut msb_position = 7;
+        let mut count : usize = 0;
+
+        while msb_position >= 0 {
+            let and_number = 1 << msb_position;
+            let is_set = and_number & preamble > 0;
+            if !is_set {
+                break;
+            }
+            count += 1;
+            msb_position -= 1;
+        }
+        // println!("preamble {} count is {}", preamble, count);
+        if count == 0 {
+            Ok(1)
+        } else if count == 1 {
+            // Invalid.
+            Result::Err("Only the MSB bit is set.")
+        } else if count > 4 {
+            Result::Err("More than 4 MSBs set in preamble.")
+        } else {
+            Ok(count)
+        }
+    }
+
     pub fn valid_utf8(data: Vec<i32>) -> bool {
-        false
+        let mut i = 0;
+
+        while i < data.len() {
+            let preamble : i32 = data[i];
+            // println!("{:#?}", preamble);
+
+            let result_or_bytes_to_validate = Solution::bytes_to_read_given_preamble(preamble);
+            if !result_or_bytes_to_validate.is_ok() {
+                return false;
+                // println!("Something wrong with the preamble {:032b}", preamble);
+            }
+
+            let bytes_to_validate = result_or_bytes_to_validate.unwrap();
+
+            if bytes_to_validate == 1 {
+                // Note this includes the already validated preamble.
+                i += 1;
+                continue;
+            }
+
+            // Invalid number of bytes to be validated.
+            // Note bytes to validate includes i.
+            if i + bytes_to_validate - 1 >= data.len() {
+                // println!("Given preamble {:032b} bytes to validate = {} i = {} overflows data with length {}", preamble, bytes_to_validate, i, data.len());
+                return false;
+            }
+
+            // Validate all the bytes.
+            for j in i+1..i+bytes_to_validate {
+                let byte : i32 = data[j];
+
+                let first_two_bits : i32 = 0xC0 & byte;
+                if first_two_bits != 0x80 {
+                    // println!("failed to validate bytes. first two bits = {} byte = {}", first_two_bits, byte);
+                    return false;
+                } else {
+                    // println!("validated bytes. first two bits = {} byte = {}", first_two_bits, byte);
+                }
+            }
+
+            i = i + bytes_to_validate;
+        }
+        return true;
     }
 }
 
@@ -61,6 +130,10 @@ fn main() {
 
     for line in fh.readAndTokenizeInput() {
         let input: Vec<i32> = line.iter().map(|s| s.parse::<i32>().unwrap()).collect();
+        println!("input = {:?}", input);
+        for num in &input {
+            println!("binary for {} is {:0b}", num, num)
+        }
         println!("{}", Solution::valid_utf8(input));
     }
 }
