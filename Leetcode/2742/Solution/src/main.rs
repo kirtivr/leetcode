9ba5cb7ed76a66cae75a0ae94958206936800fcd
kirtivr <kirtivr.bit@gmail.com>
2023-10-16 14:17:31 +0530

    leetcode 2742

--- Leetcode/2742/Solution/src/main.rs
+++ Leetcode/2742/Solution/src/main.rs
@@ -1,29 +1,105 @@
 struct Solution {}
 
+use std::cmp::min;
+use std::cmp::max;
+use std::cmp::Ordering;
+use std::convert::TryInto;
+
+// Priority Queue depends on Ord.
+impl Ord for CostMin {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let o_cost_per_time: f32 = (other.cost as f32) / (other.time as f32);
+        let s_cost_per_time: f32 = (self.cost as f32) /(self.time as f32);
+        // println!("other cost {:?} time {:?} self cost {:?} time {:?}\nother cost/time = {:?} self cost/time = {:?}", other.cost, other.time, self.cost, self.time, o_cost_per_time, s_cost_per_time);
+        if s_cost_per_time > o_cost_per_time {
+            return Ordering::Greater;
+        } else if o_cost_per_time == s_cost_per_time {
+            if self.time > other.time {
+                return Ordering::Less;
+            } else {
+                return Ordering::Greater;
+            }            
+        } else {
+            return Ordering::Less;
+        }
+    }
+}
+
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for CostMin {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(PartialEq)]
+#[derive(Eq)]
+#[derive(Debug)]
+struct CostMin {
+    cost : i32,
+    time : i32,
+    original_time : i32,
+    position : i32
+}
+
 impl Solution {
-    pub fn paint_walls(cost: Vec<i32>, time: Vec<i32>) -> i32 {
+    pub fn paint_walls(mut cost: Vec<i32>, time: Vec<i32>) -> i32 {
         // The paid painter picks the wall with the lowest cost.
         // If there are more than one wall with lowest cost, pick
         // the one that takes more time.
 
-        let cost_to_iter: std::collections::HashMap<i32, i32>;
-        for i in [0..cost.len()] {
-            cost_to_iter.insert(i, cost[i]);
-        }
-        let sorted_cost = cost;
-        sorted_cost.sort();
+        // let mut heap = BinaryHeap::new();        
+        let total_size : i32 = cost.len().try_into().unwrap();
 
-        let i = 0;
-        while i < sorted_cost.len() {
-            let lowest = sorted_cost[i];
-
-            let j = i + 1;
-            while j < sorted_cost.len() {
-
+        let mut i : usize = 0;
+        let mut sorted_container : Vec<CostMin> = vec![];
+        while i < total_size.try_into().unwrap() {
+            let mut painting_time : i32 = *time.get(i).unwrap();
+            if painting_time > total_size {
+                painting_time = total_size;
             }
+            let to_push = CostMin {
+                cost: *cost.get(i).unwrap(),
+                time : painting_time + 1,
+                original_time : *time.get(i).unwrap(),
+                position : i.try_into().unwrap()};
+            sorted_container.push(to_push);
+            // println!("At index {} sorted container = {:?}", i, sorted_container);
+            i += 1;
         }
 
-        1
+        sorted_container.sort();
+
+        println!("Sorted container before re-adjusting weights {:?}", sorted_container);
+
+        let mut walls_remaining = total_size;
+        for cost_struct in sorted_container.iter_mut() {
+            if walls_remaining <= 0 {
+                cost_struct.time = 1;
+            } else if walls_remaining <= cost_struct.time {
+                // Reduce the extraneous time.
+                let extraneous_time = cost_struct.time - walls_remaining;
+                cost_struct.time -= extraneous_time;
+                cost_struct.time = max(cost_struct.time, 1);
+            }
+            walls_remaining = walls_remaining - (cost_struct.time);
+        }
+
+        sorted_container.sort();
+        println!("Sorted container after re-adjusting weights {:?}", sorted_container);
+
+        // Based on an element's position in the sorted order, we need to update its '(effective) time' value.
+        let mut total_cost = 0;
+        let mut remaining = total_size;
+        let mut current: usize = 0;
+        while remaining > 0 {
+            let x = &sorted_container[current];
+            println!("{:?} remaining = {}", x, remaining);
+            total_cost += x.cost;
+            current += 1;
+            remaining = remaining - (1 + x.original_time);
+        }
+        total_cost
     }
 }
 
@@ -86,9 +162,20 @@ fn main() {
         for num in &input {
             println!("binary for {} is {:0b}", num, num)
         }*/
-    let cost : Vec<i32> = Vec::from([1,2,3,2]);
-    let time : Vec<i32> = Vec::from([1,2,3,2]);
+    //let cost : Vec<i32> = Vec::from([1,2,3,2]);
+    //let time : Vec<i32> = Vec::from([1,2,3,2]);
+    // let cost : Vec<i32> = Vec::from([26, 53, 10, 24, 25, 20, 107,51]);
+    // Sorted order is 10, 20, 24, 25, 26, [51], [53], [63]
+    //                   1  2   1   2   1   1   1   2
+    // let time : Vec<i32> = Vec::from([1, 1, 1, 1, 2, 2, 3, 2]);
+    // let cost : Vec<i32> = Vec::from([42,8,28,35,21,13,21,35]);
+    // let time : Vec<i32> = Vec::from([2,1,1,1,2,1,1,2]);
 
+    // let cost : Vec<i32> = Vec::from([49,35,32,20,30,12,42]);
+    // let time : Vec<i32> = Vec::from([1,1,2,2,1,1,2]);
+    let cost = Vec::from([76,25,96,46,85,19,29,88,2,5]);
+    let time = Vec::from([1,2,1,3,1,3,3,3,2,1]);
+    println!("input cost = {:?} time = {:?}", cost, time);
     println!("{}", Solution::paint_walls(cost, time));
     //}
 }
---
+++ Leetcode/2742/Solution/target/debug/deps/Solution-325b48648469d325.d
@@ -0,0 +1,5 @@
+/home/horizon/Documents/git/leetcode/Leetcode/2742/Solution/target/debug/deps/Solution-325b48648469d325: src/main.rs
+
+/home/horizon/Documents/git/leetcode/Leetcode/2742/Solution/target/debug/deps/Solution-325b48648469d325.d: src/main.rs
+
+src/main.rs:
