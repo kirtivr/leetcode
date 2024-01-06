caf3551d0d85737b2f4aa5b135274ba5315c4dd5
kirtivr <kirtivr.bit@gmail.com>
2024-01-06 13:40:11 +0530

    Brainstorm on 1856

--- Leetcode/1856_new.py
+++ Leetcode/1856_new.py
@@ -5,6 +5,19 @@ import time
 
 class Solution:
     def maxSumMinProduct(self, nums: List[int]) -> int:
+        # For any subarray from i to j we want to find the maximum
+        # (j - i + 1) * minimum(element in that subarray).
+        # Our focus has to be on the span - the number of elements
+        # and the minimum element in that span.
+
+        # Checking all subarrays is O(n^2) time, which is too much with
+        # 10^5 elements. We want to avoid checking all subarrays.
+
+        # Idea (test to verify if this works):
+        # For each index i, have a map called 'left' which stores the index of the left element less than nums[i].
+        # Do the same with 'right'.
+        # This is how we get the 'span' or the range for a given element where it is the minimum.
+        # Then find the product for each index, and store the maximum.
         max_product = 0
         i = 0
         j = 1
@@ -21,6 +34,12 @@ class Solution:
 
             total_sum += right
             # Case where right is not suitable.
+            # The idea is this:
+            # We have a subarray [a, b, c, d, e, f, g, h]. c is the minimum element.
+            # The product is 8 * c.
+            # Now we added another element t.
+            # t << c.
+            # Now the product is 9 * t which is less than 8 * c.
             if right < nums[min_idx] and (j - i + 1)*(nums[min_idx] - right) > right:
                 i = j
                 j += 1
